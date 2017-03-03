ac0e6fa1fea9b32ae8760b846a21d75d24db34e3
Rob Sanderson <azaroth42@gmail.com>
2017-03-03 15:28:24 -0800

    auto-update

--- scripts/mk_examples.py
+++ scripts/mk_examples.py
@@ -12,7 +12,7 @@ from cromulent.vocab import Painting, InformationObject, Department, SupportPart
 	Attribution, Appraising, Dating, AuctionHouse, Auction, Bidding, AuctionCatalog, \
 	LotNumber, Auctioneer, Bidding, AuctionLotSet, Theft, LocalNumber, AccessionNumber, \
 	PrimaryTitle, Sculpture, Description, Width, Height, DimensionStatement, \
-	CreditStatement, RightsStatement, WebPage, \
+	CreditStatement, RightsStatement, WebPage, Sculpture, \
 	materialTypes, dimensionUnits
 from cromulent.extra import PhysicalObject, Payment, DestructionActivity, add_rdf_value, \
 	add_schema_properties
