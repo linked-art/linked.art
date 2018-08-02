240f9447e7522a94bebc3edcb98e6905b57cb30b
Rob Sanderson <azaroth42@gmail.com>
2018-08-02 13:19:19 -0700

    Document inferred resources in assertion/

--- build.sh
+++ build.sh
@@ -5,13 +5,17 @@ rm -rf .site/*
 
 # Clear any old example copies
 rm content/example/activity/*
+rm content/example/concept/*
 rm content/example/group/*
 rm content/example/identifier/*
-rm content/example/text/*
+rm content/example/legal/*
+rm content/example/name/*
 rm content/example/object/*
 rm content/example/person/*
 rm content/example/place/*
+rm content/example/set/*
 rm content/example/text/*
+rm content/example/value/*
 rm content/model/example_index.html
 
 # Remake the table, still separate
