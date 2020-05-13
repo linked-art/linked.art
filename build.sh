e5d405c45f5f6e04133b8dc18e10e32ea7c8e005
Richard Palmer <r.palmer@vam.ac.uk>
2020-05-13 12:43:19 +0100

    Update directory to deploy

--- build.sh
+++ build.sh
@@ -4,26 +4,26 @@
 rm -rf .site/*
 
 # Clear any old example copies
-rm content/example/activity/*
-rm content/example/concept/*
-rm content/example/group/*
-rm content/example/identifier/*
-rm content/example/legal/*
-rm content/example/name/*
-rm content/example/object/*
-rm content/example/person/*
-rm content/example/place/*
-rm content/example/set/*
-rm content/example/text/*
-rm content/example/value/*
-rm content/model/example_index.html
+#rm content/example/activity/*
+#rm content/example/concept/*
+#rm content/example/group/*
+#rm content/example/identifier/*
+#rm content/example/legal/*
+#rm content/example/name/*
+#rm content/example/object/*
+#rm content/example/person/*
+#rm content/example/place/*
+#rm content/example/set/*
+#rm content/example/text/*
+#rm content/example/value/*
+#rm content/model/example_index.html
 
 # Remake the table, still separate
-python scripts/mk_table.py
+#python scripts/mk_table.py
 
 # Split up the current ontology into little static files
-rm content/ns/terms/*
-python scripts/split_ontology.py
+#rm content/ns/terms/*
+#python scripts/split_ontology.py
 
 # And now generate the site from scratch
 mkdocs build --clean
